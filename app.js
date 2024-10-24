a16a6a3429dd067b2681dd9a536081d7f98e7f03
kearcarlos <100732533+kearcarlos@users.noreply.github.com>
2024-10-24 16:21:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
+// CODI ÚTIL
+
+let lista = [
+  { nombre: "JS", funcionalidad: "back y front", caracteristicas: "flexible" },
+  {
+    nombre: "Java",
+    funcionalidad: "back y front",
+    caracteristicas: "intransigente",
+  },
+  { nombre: "Python", funcionalidad: "back", caracteristicas: "flexible" },
+  { nombre: "PHP", funcionalidad: "back", caracteristicas: "flexible" },
+  { nombre: "C++", funcionalidad: "back", caracteristicas: "intransigente" },
+];
+
+
 app.get('/', (req, res) => res.send('Hello Upgrad v2 :)'));
+
+app.get("/hola", (req, res) => {
+  let frontend = `<div id="print" style="display: flex; flex-wrap: wrap">`;
+  for (let i = 0; i < lista.length; i++) {
+    frontend += `
+    <div class="card" style="width: 10%; border: 2px black solid; margin: 5px; text-align: center">
+    <h1>${lista[i].nombre}</h1>
+    <p>${lista[i].funcionalidad}</p>
+    <p>${lista[i].caracteristicas}</p>
+    </div>`;
+  }
+  frontend += `</div>`;
+  res.send(frontend);
+});
+
+
+
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
